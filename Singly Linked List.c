b700948de1dad6574034f711e0833a2e0b54eed4
Debajyoti Das <136669104+CoderDebajyoti@users.noreply.github.com>
2024-09-05 01:02:09 +0530

    Update Singly Linked List.c

--- Singly Linked List.c
+++ Singly Linked List.c
@@ -10,99 +10,109 @@ struct node
 // Global head pointer
 struct node *head = NULL;
 
+// Function to create and initialize a new node
+struct node* create_node(int item)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL) // Check if memory allocation was successful
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    ptr->data = item;
+    ptr->link = NULL;
+    return ptr;
+}
+
 void insert_at_beg()
 {
-    struct node *ptr = (struct node *)malloc(sizeof(struct node));//ptr==new node
     int item;
     printf("Enter the data of new Node: ");
     scanf("%d", &item);
-    ptr->data = item;
-    ptr->link = NULL;
+    struct node *ptr = create_node(item); // Create and initialize new node
 
     if (head == NULL)
     {
-        head = ptr;
+        head = ptr; // Empty list
     }
     else
     {
-        ptr->link = head;
-        head = ptr;
+        ptr->link = head; // New node points to old head
+        head = ptr; // Head now points to the new node
     }
-    free(ptr);
 }
 
 void insert_at_end()
 {
-	struct node *ptr = (struct node *)malloc(sizeof(struct node));
-	struct node *ptr1 = (struct node *)malloc(sizeof(struct node));
     int item;
     printf("Enter the data of new Node: ");
     scanf("%d", &item);
-    ptr->data = item;
-    ptr->link = NULL;
+    struct node *ptr = create_node(item); // Create and initialize new node
+    struct node *ptr1 = head; // Pointer to traverse the list
     
-	if (head == NULL)
+    if (head == NULL)
     {
-        head = ptr;
+        head = ptr; // Empty list
     }
-    else if(head->link == NULL)
+    else
     {
-    	head->link=ptr;
-	}
-	else
-	{
-		ptr1 = head;
-		while(ptr1->link != NULL)
-		{
-			ptr1=ptr1->link;
-		}
-		ptr1->link=ptr;
-	}
-	free(ptr);
-	free(ptr1);
+        while (ptr1->link != NULL) // Traverse to the end of the list
+        {
+            ptr1 = ptr1->link;
+        }
+        ptr1->link = ptr; // Last node points to new node
+    }
 }
 
 void insert_at_any()
 {
-	struct node *ptr = (struct node *)malloc(sizeof(struct node));//new node
-	struct node *ptr1 = (struct node *)malloc(sizeof(struct node));//pointer for traverse
-    int item,pos;
+    int item, pos;
     printf("Enter the data of new Node: ");
     scanf("%d", &item);
-    ptr->data = item;
-    ptr->link = NULL;
     printf("Enter the position of new Node: ");
     scanf("%d", &pos);
-    if(ptr==NULL)
-    {
-    	printf("Not possible.");
-    	break;
-	}
-	if(pos==1)
-	{
-		insert_at_beg();
-	}
-	else
-	{
-		int count=1;
-		*ptr1 = head;
-		while (ptr1 != NULL && count < pos-1)
-    {
-        ptr1 = ptr1->link;
-        count = count +1;
-    }
     
-    if (ptr->link == && count != pos-1)
+    struct node *ptr = create_node(item); // Create and initialize new node
+    struct node *ptr1 = head; // Pointer for traversal
+
+    if (pos == 1)
     {
-    	printf("position not exist");
-    	break;
-	}
-	ptr->link = ptr1->link;
-	ptr1->link = ptr;
-	printf("%d",ptr->data);
-	free(ptr);
-	free(ptr1);
-	}
+        insert_at_beg(); // Insert at beginning if position is 1
+    }
+    else
+    {
+        int count = 1;
+        while (ptr1 != NULL && count < pos - 1)
+        {
+            ptr1 = ptr1->link;
+            count++;
+        }
+        
+        if (ptr1 == NULL)
+        {
+            printf("Position does not exist.\n");
+        }
+        else
+        {
+            ptr->link = ptr1->link;
+            ptr1->link = ptr;
+        }
+    }
+}
+
+void delete_from_beg()
+{
+    if (head == NULL)
+    {
+        printf("List does not exist.\n");
+    }
+    else
+    {
+        struct node *ptr = head; // Pointer to the node to be deleted
+        head = ptr->link;
+        printf("Deleted Node data: %d\n", ptr->data);
+        free(ptr);
+    }
 }
 
 void display()
@@ -122,7 +132,7 @@ int main()
 
     do
     {
-        printf("\nMenu:\n1. Insert at Beginning\n2. Insert at End\n3. Insert at any position\n7. Display\n8. Exit\n");
+        printf("\nMenu:\n1. Insert at Beginning\n2. Insert at End\n3. Insert at any position\n4. Delete from Beginning\n5. Delete at End\n6. Delete at any position\n7. Display\n8. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -137,14 +147,14 @@ int main()
         case 3:
             insert_at_any();
             break;  
-		case 4:
-            //delete_at_beg();
+        case 4:
+            delete_from_beg();
             break; 
-		case 5:
-            //delete_at_end();
+        case 5:
+            // Implement delete_at_end() function
             break;
-		case 6:
-            //delete_at_any();
+        case 6:
+            // Implement delete_at_any() function
             break; 
         case 7:
             display();
@@ -155,7 +165,7 @@ int main()
         default:
             printf("Invalid choice! Please try again.\n");
         }
-    } while (1);
+    } while (choice != 8); // Exit the loop when choice is 8
 
     return 0;
 }
